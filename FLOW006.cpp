f7e51a40d1989cf6c6cfd8d4a951ff41fa2b892e
Mrunal Jambenal <79159212+mrunal736@users.noreply.github.com>
2021-07-13 15:55:36 +0530

    Create FLOW006.cpp

---
+++ FLOW006.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	int n,sum=0;
+	cin>>n;
+	while(n!=0){
+	    sum += n%10;
+	    n = n/10;
+	}
+	cout<<sum<<endl;
+	}
+	return 0;
+}
