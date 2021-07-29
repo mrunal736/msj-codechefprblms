318ab20713cfb04666cef860843dd188ebb86b75
Mrunal Jambenal <79159212+mrunal736@users.noreply.github.com>
2021-07-29 19:48:12 +0530

    Create CHSFORMT.cpp

---
+++ CHSFORMT.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int a,b;
+	    cin>>a>>b;
+	    int res = a + b;
+	    if(res<3){
+	        cout<<"1\n";
+	    }
+	    else if(res>=3 && res<=10){
+	        cout<<"2\n";
+	    }
+	    else if(res>=11 && res<=60){
+	        cout<<"3\n";
+	    }
+	    else if(res>60){
+	        cout<<"4\n";
+	    }
+	}
+	return 0;
+}
